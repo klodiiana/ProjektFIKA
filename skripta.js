26c0ec0db806c28934b96859ff2ba575e4150afb
klodiiana <95685448+klodiiana@users.noreply.github.com>
2021-12-23 01:19:04 +0100

    books

--- skripta.js
+++ skripta.js
@@ -12,3 +12,31 @@ new Swiper('.home-slider', {
     },
     speed: 1000
 });
+
+var swiper = new Swiper('.books-slider', {
+    spaceBetween: 10,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      450: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+    },
+  });
