b4a15a0ba21642bf3966762b32a9b46d51df9744
klodiiana <95685448+klodiiana@users.noreply.github.com>
2021-12-28 00:32:48 +0100

    28.12.21

--- skripta.js
+++ skripta.js
@@ -1,47 +1,24 @@
-var searchForm = document.querySelector('.search-form');
+var searchForm = document.querySelector(".search-form");
 
 searchForm.onclick = () => {
-    searchForm.classList.toggle('active');
-}
+  searchForm.classList.toggle("active");
+};
 
-let loginForm = document.querySelector('.login-form');
+let loginForm = document.querySelector(".login-form");
 
-document.querySelector('#login-btn').onclick = () => {
-  loginForm.classList.toggle('active');
-}
+document.querySelector("#login-btn").onclick = () => {
+  loginForm.classList.toggle("active");
+};
 
-document.querySelector('#close-login-btn').onclick = () => {
-  loginForm.classList.remove('active');
-}
+document.querySelector("#close-login-btn").onclick = () => {
+  loginForm.classList.remove("active");
+};
 
-
-
-
-new Swiper('.home-slider', {
-    loop: true,
-    effect: "fade",
-    autoplay: {
-        delay: 2000
-    },
-    speed: 1000
-});
-
-var swiper = new Swiper(".books-slider", {
-  loop:true,
-  centeredSlides: true,
+new Swiper(".home-slider", {
+  loop: true,
+  effect: "fade",
   autoplay: {
-      delay: 9500,
-      disableOnInteraction: false,
+    delay: 2000,
   },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    1024: {
-      slidesPerView: 3,
-    },
-  },
+  speed: 1000,
 });
