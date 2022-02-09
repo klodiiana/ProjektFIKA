ff3a256e986114dea6baab03ab298d814fd50f2b
klodiiana <95685448+klodiiana@users.noreply.github.com>
2022-02-09 19:24:39 +0100

    editime te vogla

--- account.js
+++ account.js
@@ -2,10 +2,10 @@ const login_btn = document.querySelector("#login-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
 
-sign_up_btn.addEventListener('click', () => {
-    container.classList.add("sign-up-mode");
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
 });
 
-login_btn.addEventListener('click', () => {
-    container.classList.remove("sign-up-mode");
+login_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
 });
