d2e9f329256d4c6a7210f33e07cf9e5fbb76b952
klodiiana <95685448+klodiiana@users.noreply.github.com>
2022-02-09 16:51:48 +0100

    updated login/signup form

---
+++ account.js
@@ -0,0 +1,11 @@
+const login_btn = document.querySelector("#login-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener('click', () => {
+    container.classList.add("sign-up-mode");
+});
+
+login_btn.addEventListener('click', () => {
+    container.classList.remove("sign-up-mode");
+});
--- skripta.js
+++ skripta.js
@@ -3,17 +3,6 @@ var searchForm = document.querySelector(".search-form");
 searchForm.onclick = () => {
   searchForm.classList.toggle("active");
 };
-
-let loginForm = document.querySelector(".login-form");
-
-document.querySelector("#login-btn").onclick = () => {
-  loginForm.classList.toggle("active");
-};
-
-document.querySelector("#close-login-btn").onclick = () => {
-  loginForm.classList.remove("active");
-};
-
 new Swiper(".home-slider", {
   loop: true,
   effect: "fade",
